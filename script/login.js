2f0f3df783e41ead092bed4323b039dc9cfdc13c
raselmondol2344 <raselmondream245@gmail.com>
2026-02-20 10:29:48 +0600

    login page complete

---
+++ script/login.js
@@ -0,0 +1,23 @@
+document.getElementById("login-btn").addEventListener("click", function()
+{
+    const inputNum = document.getElementById("login-number")
+    const Number = inputNum.value;
+    console.log(Number);
+
+
+    const inputPin = document.getElementById("login-pin")
+    const pin = inputPin.value;
+    console.log(pin);
+
+    if(Number == "01703114263" && pin == "1234")
+    {
+        alert (" login success")
+        window.location.assign("/home.html");
+    }
+
+    else {
+        alert("login faild")
+        return;
+    }
+}
+)
